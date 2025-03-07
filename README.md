4c4a4cb9433ce7e3ea54c14fa4d5583c829da398
unknown <zaidsmadanat@gmail.com>
2025-03-07 15:52:32 -0800

    added multisite traversal, project complete

---
+++ README.md
@@ -0,0 +1,4 @@
+## Basic Informational Site
+Basic site to navigate through a local server. 
+If the user navigates to an undefined site, they will be greeted with a 404 error. 
+First NodeJS project with The Odin Project
--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-// const axios = require('axios');
 const http = require('http');
-let fs = require('fs')
+const fs = require('fs');
+const { URL } = require('url');
 
+/**
+ * Checks filename and returns corresponding page dynamically. 
+ * @param {*} req 
+ * @param {*} res 
+ */
+const requestListener = (req, res) => {
+    const filePath = new URL(req.url, 'http://localhost');
+    let filename = "";
+    console.log(filename)
+    if (filePath.pathname === "/") { 
+        filename = "index.html"; 
+    }
+    else if (filePath.pathname === "/contact-me") { 
+        filename = "contact-me.html";
+    }
+    else if (filePath.pathname === "/about") { 
+        filename = "about.html";
+    }
+    else { 
+        filename = "404.html";
+    }
 
-
-const requestListener = (req, res) => { 
-    fs.readFile('index.html', (err, data) => {
+    fs.readFile(filename, (err, data) => {
         res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data); 
             return res.end();
